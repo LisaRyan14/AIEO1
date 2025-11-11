fad8e4ed9292b3709b01cbd1876ea35c136da1e5
lisaryan14 <lisa.ryan@post.harvard.edu>
2025-11-11 18:42:49 -0500

    moved game to it's own repo

--- 8_bit_ball_game/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1dOVVEHB4toCQDZ85CJeFm9V+cX7JhX7s2GjEyFxtAY=\"\n}"
--- 8_bit_ball_game/.next/dev/static/chunks/app_page_tsx_1b1447d5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_56974a2a._.js",
-  "static/chunks/_a378fd57._.js"
-],
-    source: "dynamic"
-});
